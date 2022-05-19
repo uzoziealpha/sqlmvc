c8fbada491b10e8897221f9db48d3f2e174b9eb1
Obi b. Uzozie <uzozie.obi@icloud.com>
2022-05-19 09:27:31 +0800

    seeding data

--- MVCLibrary/Program.cs
+++ MVCLibrary/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MVCLibrary.Data;
+using MVCLibrary.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<LibraryContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryContext") ?? throw new InvalidOperationException("Connection string 'LibraryContext' not found.")));
@@ -16,6 +18,13 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
 }
 
 app.UseHttpsRedirection();
